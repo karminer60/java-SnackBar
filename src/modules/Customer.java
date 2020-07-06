e351600f12f21c0f9525401b02abf94bf168ae26
karina-rodriguez <karminer60@gmail.com>
2020-07-06 14:21:07 -0700

    created instances of class customer

--- src/modules/Customer.java
+++ src/modules/Customer.java
@@ -6,5 +6,35 @@ public class Customer
 	
 	private String fname;
 	private int amount;
-    
+
+	public Employee(String fname, int amount)
+	{
+		maxId++;
+		id =maxId;
+
+		this.fname = fname;
+		
+		this.amount = amount;
+	}
+
+ 	public String getFname()
+	{
+		return fname;
+	}  
+
+	public void setFname(String fname)
+	{
+		this.fname = fname;
+		//this-the fields that we are dealing with 
+	} 
+
+	public int getAmount()
+	{
+		return companyId;
+	}
+
+	public void setAmount(int amount)
+	{
+		this.amount = amount;
+	}
 }
